fd7baddeacea963c1a5f1582760ae95dda6d8c4c
Lakshmi <lakshmi.g@giglabz.com>
2025-10-21 12:28:02 +0530

    Initial Commit

---
+++ MiniSwitUI/MiniSwitUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniSwitUIApp.swift
+//  MiniSwitUI
+//
+//  Created by Lakshmi Sarath on 21/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MiniSwitUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
